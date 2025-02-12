2447e103d4cc0fd34496e4e3d5e4435ff7b5d10d
KibumKim92 <kk3682@cumc.columbia.edu>
2025-02-12 15:00:56 -0500

    Add files via upload

--- Function/getFilenames.m
+++ Function/getFilenames.m
@@ -1,13 +1,7 @@
-function filenames=getFilenames(path,regExp)
+function filenames=getFilenames(path)
 % returns a list of the filenames in a specified directory
-% the second parameter can be used to specify a regular expression required
-% in the filenames
 
 d0=dir(path);
 filenames={d0([d0.isdir]==0).name};
 filenames=filenames(:);
 filenames=setdiff(filenames,{'Thumbs.db','thumbs.db'});
-
-if nargin>1
-    filenames=filenames(boolRegExp(filenames,regExp));
-end
